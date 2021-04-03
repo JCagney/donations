5751aa93288bcfa1f63f398d7b3d4d060e00a204
John Cagney <59435586+JCagney@users.noreply.github.com>
2021-04-03 17:16:19 +0100

    api

--- app/api/candidates.js
+++ app/api/candidates.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Candidate = require('../models/candidate');
+const Boom = require("@hapi/boom");
 
 const Candidates = {
   find: {
@@ -8,6 +9,20 @@ const Candidates = {
     handler: async function (request, h) {
       const candidates = await Candidate.find();
       return candidates;
+    },
+  },
+  findOne: {
+    auth: false,
+    handler: async function (request, h) {
+      try {
+        const candidate = await Candidate.findOne({ _id: request.params.id });
+        if (!candidate) {
+          return Boom.notFound("No Candidate with this id");
+        }
+        return candidate;
+      } catch (err) {
+        return Boom.notFound("No Candidate with this id");
+      }
     },
   },
 };
